5383338b72930c473197b0a5230a2cb5e7781d2f
jissmon <jisshub>
2020-10-12 18:39:31 +0530

    outputting posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -48,8 +48,7 @@ export class AppComponent implements OnInit {
           return postsArray;
        }))
        .subscribe(posts => {
-         this.loadedPosts = posts;
-         
-       })
+         this.loadedPosts = posts;   
+       });
   }
 }
