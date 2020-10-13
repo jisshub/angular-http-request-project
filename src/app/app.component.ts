1bdf11688d09070717d49e8f055eab5ae5ca535a
jissmon <jisshub>
2020-10-13 18:47:45 +0530

    handled errors usng subjects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Post } from "./post.model";
 import { PostService } from './post.service';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'angular-http-request-project'; 
-   loadedPosts: Object[]= [];
+  loadedPosts: Object[]= [];
+  error = null;
+  private errSub: Subscription
   constructor(private http: HttpClient, private postService: PostService) {}
-
   ngOnInit() {
+    //  subscribe to errSub property 
+    this.errSub = this.postService.errObject.subscribe(errMsg => {
+        this.error = errMsg;
+    })
     this.postService.FetchPosts().subscribe(posts => {
       console.log(posts);
       // assign posts to empty array loadedPosts
       this.loadedPosts = posts;
+    }, error => {
+      this.error = error.message;
+      console.log(error);
     });
   }
 
@@ -38,5 +47,9 @@ export class AppComponent implements OnInit {
     this.postService.DeletePosts().subscribe(() => {
         this.loadedPosts= [];
     })
+  }
+  ngOnDestroy(){
+    // unsubscribe 
+    this.errSub.unsubscribe();
   }
 }
